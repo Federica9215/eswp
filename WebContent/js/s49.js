478160e7d46e96b7452f0b38f552960f83e236bd
Administrator <Administrator@192.168.12.52>
2019-08-27 17:52:46 +0200

    Nuovo

--- WebContent/js/s49.js
+++ WebContent/js/s49.js
@@ -8,7 +8,7 @@ $(function() {
     
     $('a').click(function(event) {
         alert('You should not use any link on this page!');
-        event.preventDefault();
+        event.preventDefault(); //fai quello che ti dico di fare e nn qll che faresti di solito
     });
 
     $('html').dblclick(function(e) {
